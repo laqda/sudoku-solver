5c12f3d50c1d7372ada45fa909cb301263ae3965
Quentin Michel <quentinmichel69110@gmail.com>
2019-05-15 22:15:23 +0200

    support 16*16

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,38 @@
 public class Main {
 
     public static void main(String[] args) {
+//        Grid grid = new Grid(9);
+//
+//        grid.setCellValue(0, 5, 5);
+//        grid.setCellValue(8, 8, 1);
+//
+//        // test erase
+//        grid.setCellValue(2, 3, 0);
+//        grid.setCellValue(2, 3, 0);
+//
+//        // whole square
+//        grid.setCellValue(3, 3, 1);
+//        grid.setCellValue(4, 3, 2);
+//        grid.setCellValue(5, 3, 3);
+//        grid.setCellValue(3, 4, 4);
+//        grid.setCellValue(4, 4, 5);
+//        grid.setCellValue(5, 4, 6);
+//        grid.setCellValue(3, 5, 7);
+//        grid.setCellValue(4, 5, 8);
+//        grid.setCellValue(5, 5, 9);
+//
+//        try {
+//            grid.display();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+
+        Grid grid16 = new Grid(16);
+
+        grid16.setCellValue(12, 0, 11);
+
         try {
-            Grid grid = new Grid(9);
-
-            grid.setCellValue(0, 5, 5);
-            grid.setCellValue(8, 8, 1);
-
-            // test erase
-            grid.setCellValue(2, 3, 0);
-            grid.setCellValue(2, 3, 0);
-
-            // whole square
-            grid.setCellValue(3, 3, 1);
-            grid.setCellValue(4, 3, 2);
-            grid.setCellValue(5, 3, 3);
-            grid.setCellValue(3, 4, 4);
-            grid.setCellValue(4, 4, 5);
-            grid.setCellValue(5, 4, 6);
-            grid.setCellValue(3, 5, 7);
-            grid.setCellValue(4, 5, 8);
-            grid.setCellValue(5, 5, 9);
-
-            grid.display();
+            grid16.display();
         } catch (Exception e) {
             e.printStackTrace();
         }
