1b3a0cb5d02b1a4d2b3dcf40c273c146ee60b5e6
Quentin Michel <quentinmichel69110@gmail.com>
2019-05-17 10:43:42 +0200

    more realistic speeds

--- src/consts/Configuration.java
+++ src/consts/Configuration.java
@@ -5,7 +5,7 @@ public class Configuration {
     public static final int TIMER_WAIT = 1;
 
     public enum SPEED {
-        LOW(1000), MEDIUM(700), FAST(300), STEP_BY_STEP;
+        LOW(1000), MEDIUM(100), FAST(10), STEP_BY_STEP;
         private int speed;
 
         SPEED() {
