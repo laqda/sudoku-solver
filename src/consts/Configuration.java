e9b903b31696146150d5972544d5780826749b66
Quentin Michel <quentinmichel69110@gmail.com>
2019-05-17 10:44:16 +0200

    remove unecessary configuration

--- src/consts/Configuration.java
+++ src/consts/Configuration.java
@@ -2,8 +2,6 @@ package consts;
 
 public class Configuration {
 
-    public static final int TIMER_WAIT = 1;
-
     public enum SPEED {
         LOW(1000), MEDIUM(100), FAST(10), STEP_BY_STEP;
         private int speed;
