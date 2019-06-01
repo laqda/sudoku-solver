c2d25d88dc8624f0c211fd508a6763fc91dececb
Quentin Michel <quentinmichel69110@gmail.com>
2019-06-01 16:43:47 +0200

    reformat code

--- src/analysis/GraphComparator.java
+++ src/analysis/GraphComparator.java
@@ -1,16 +1,12 @@
 package analysis;
 
-
-import generator.Generator;
-import grid.DefaultGrid;
 import grid.Grid;
-import solver.GraphGenerator;
-import solver.GraphGenerator.*;
 import tree.Node;
 
 import static solver.GraphGenerator.*;
 
 public class GraphComparator {
+
     public static void main(String[] args) {
         long diff;
         //Grid rootGrid = new DefaultGrid(9);
@@ -22,36 +18,31 @@ public class GraphComparator {
 
         long startRC = System.currentTimeMillis();
         System.out.println("Generating reduced universe w. constraint graph");
-        Node ndRC = generateReducedUniverseWithConstraintVerification(rootGrid,  false);
+        Node ndRC = generateReducedUniverseWithConstraintVerification(rootGrid, false);
         long endRC = System.currentTimeMillis();
         diff = endRC - startRC;
         System.out.println("Finished in : " + diff + "ms");
 
         long startFC = System.currentTimeMillis();
         System.out.println("Generating full universe w. constraint graph");
-        Node ndFC = generateFullUniverseWithConstraintVerification(rootGrid,  false);
+        Node ndFC = generateFullUniverseWithConstraintVerification(rootGrid, false);
         long endFC = System.currentTimeMillis();
         diff = endFC - startFC;
         System.out.println("Finished in : " + diff + "ms");
 
         long startR = System.currentTimeMillis();
         System.out.println("Generating reduced universe graph");
-        Node ndR = generateReducedUniverse(rootGrid,  false);
+        Node ndR = generateReducedUniverse(rootGrid, false);
         long endR = System.currentTimeMillis();
         diff = endR - startR;
         System.out.println("Finished in : " + diff + "ms");
 
-
-
-
         long startF = System.currentTimeMillis();
         System.out.println("Generating full universe graph");
-        Node ndF = generateFullUniverse(rootGrid,  false);
+        Node ndF = generateFullUniverse(rootGrid, false);
         long endF = System.currentTimeMillis();
         diff = endF - startF;
         System.out.println("Finished in : " + diff + "ms");
-
-
-
     }
+
 }
